9f6c015fa0ca2319293fa1039b41e4affaebf3bb
Clémence Lesné <clemence@lesne.pro>
2024-06-14 16:21:49 +0200

    breaking: Use multiple destinations
    refacto: Manage SDK instanced in config models

--- helpers/config_models/ai_search.py
+++
@@ -1,7 +0,0 @@
-from pydantic import SecretStr, BaseModel
-
-
-class AiSearchModel(BaseModel):
-    access_key: SecretStr
-    endpoint: str
-    index: str
--- helpers/config_models/llm.py
+++ helpers/config_models/llm.py
@@ -1,14 +1,47 @@
+from enum import Enum
+from openai import AsyncAzureOpenAI
 from pydantic import SecretStr, BaseModel
+from typing import Optional
 
 
-class ConfigModel(BaseModel):
+class TypeEnum(Enum):
+    FAST = "fast"
+    SLOW = "slow"
+
+
+class ConfigModel(BaseModel, frozen=True):
+    _client: Optional[AsyncAzureOpenAI] = None
     api_key: SecretStr
     context: int
     deployment: str
     endpoint: str
     model: str
+    type: TypeEnum
+
+    async def instance(self) -> tuple[AsyncAzureOpenAI, "ConfigModel"]:
+        if not self._client:
+            self._client = AsyncAzureOpenAI(
+                # Deployment
+                api_version="2023-12-01-preview",
+                azure_deployment=self.deployment,
+                azure_endpoint=self.endpoint,
+                # Reliability
+                max_retries=30,  # We are patient, this is a background job :)
+                timeout=180,  # 3 minutes
+                # Authentication
+                api_key=self.api_key.get_secret_value(),
+            )
+        return self._client, self
+
+
+class FastModel(ConfigModel):
+    type: TypeEnum = TypeEnum.FAST
+
+
+class SlowModel(ConfigModel):
+    type: TypeEnum = TypeEnum.SLOW
 
 
 class LlmModel(BaseModel):
-    fast: ConfigModel
-    slow: ConfigModel
+    fast: FastModel
+    slow: SlowModel
